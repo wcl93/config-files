ccc91cfd93c72af0c49d69dacb6a42758b521d9e
Wayne Luong <wayneluong93@gmail.com>
2018-08-14 20:34:31 +0100

    Rename webpack.config.js to webpack2.config.js
    React sass webpack config

---
+++ webpack2.config.js
@@ -0,0 +1,54 @@
+/* webpack.config.js */
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+/* webpack.config.js */
+
+
+module.exports = {
+    // Tell webpack to begin building its 
+    // dependency graph from this file.
+    entry: path.join(__dirname, 'src', 'components', 'App.js'),
+    // And to place the output in the `build` directory
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    /* ... */
+    module: {
+        rules: [{
+                test: /\.jsx?$/,
+                /* We'll leave npm packages as is and not 
+                   parse them with Babel since most of them 
+                   are already pre-transpiled anyway. */
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            /* ... */
+            {
+                test: /\.(jpe?g|png|gif)$/,
+                use: [{
+                    /* inline if smaller than 10 KB, otherwise load as a file */
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000
+                    }
+                }]
+            },
+            {
+                test: /\.(eot|svg|ttf|woff2?|otf)$/,
+                use: 'file-loader'
+            }
+        ]
+    },
+    /* ... */
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.join(__dirname, 'public', 'index.html')
+        })
+    ]
+}
