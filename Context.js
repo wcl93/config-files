c379e50aef04e1e4a9c94c12c3361d28476d9346
Wayne Luong <wayneluong93@gmail.com>
2018-10-10 13:00:11 +0100

    Update Context.js

--- Context.js
+++ Context.js
@@ -5,13 +5,27 @@ const Context = React.createContext();
 
 const reducer = (state, action)=> {
     switch(action.type){
-        case 'EXAMPLE':
-        return (
-            console.log('Action success')
-        );
-        case 'EXAMPLE2':
-        return{
+        case 'DELETE_CONTACT':
+        return {
+            ...state, 
+            contacts:state.contacts.filter(contact=>
+            contact.id !== action.payload) 
         };
+        case 'ADD_CONTACT':
+        return {
+            ...state,
+            contacts: [action.payload, 
+                ...state.contacts]
+        };
+        case 'UPDATE_CONTACT':
+        return {
+            ...state,
+            //  this === this ? IF : ELSE
+            //id comes from payload.id response 
+            contacts:state.contacts.map(contact =>
+                contact.id === action.payload.id ? (contact = 
+                    action.payload ):contact) 
+        }
         default:
         return state;
     }
