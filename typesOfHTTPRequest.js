b0c6cfcf896785ce492dcfb7c395c2a2d492fe32
Wayne Luong <wayneluong93@gmail.com>
2018-10-12 19:52:26 +0100

    Update typesOfHTTPRequest.js

--- typesOfHTTPRequest.js
+++ typesOfHTTPRequest.js
@@ -3,6 +3,9 @@ const res = await axios.post('https://jsonplaceholder.typicode.com/users',
 newContact)
   dispatch({ type: "ADD_CONTACT", 
   payload: res.data })
+----------
+ const res = await axios.get('https://jsonplaceholder.typicode.com/users')
+ this.setState({contacts:res.data})
 
 /* -------------------FETCH------------------- */
 const url = 'https://jsonplaceholder.typicode.com/post';
@@ -10,17 +13,32 @@ const url = 'https://jsonplaceholder.typicode.com/post';
   fetch(url)
   .then (data=>{return data.json()})
   .then(res=>{console.log(res)})
+-------------
+fetch(url)
+      .then(data => {
+        return data.json();
+      })
+      .then(res => {
+        this.setState({ contacts: res });
+      });
 
   /* ----------------AJAX----------------------------- */
 const HTTP =  new XMLHttpRequest();
 const url = 'https://jsonplaceholder.typicode.com/post';
 
 HTTP.open('GET', url);
+HTTP.onload =()=>{
+            console.log(JSON.parse(HTTP.responseText));
+        }
+        HTTP.send();
 HTTP.send();
-
-HTTP.onreadystatechange=(e)=>{
-    console.log(HTTP.responseText)
-}
+---------------
+  const http = new XMLHttpRequest();
+    http.open("GET", url);
+    http.onload = () => {
+      this.setState({ contacts: JSON.parse(http.responseText) });
+    };
+    http.send();
 
 /* ----------------------jQuery AJAX--------------------- */
 $(document.readyState(function(){
@@ -37,6 +55,18 @@ $(document.readyState(function(){
         }
     })
 }))
+---------------
+    import $ from "jquery";
+    $.ajax({
+      url: url,
+      type: "GET",
+      success: result => {
+        this.setState({ contacts: result });
+      },
+      error: function(error) {
+        console.log(`Error: ${error}`);
+      }
+    });
 
 /* JSON.stringify - Turns object into string to send to server
 JSON.parse - Turns JSON data to object*/
